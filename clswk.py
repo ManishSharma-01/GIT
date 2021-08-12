3d23322135eeab5af3e497e9d2d226d4b4b63b2b
Manish <27.maanish@gmail.com>
2021-08-12 13:06:33 +0545

    update

--- clswk.py
+++ clswk.py
@@ -2,7 +2,8 @@ from tkinter import *
 
 win = Tk()
 win.title("Calculator")
-
+p1=PhotoImage(file="calc.png")
+win.iconphoto(False, p1)
 
 def btn_click(item):
     global expression
@@ -30,7 +31,7 @@ input_frame = Frame(win, width=300, height=50, bd=0, highlightbackground="lightc
 input_frame.pack(side=TOP)
 
 #SCREEN
-input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=25, bg="#eee", bd=0,
+input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=25, bg="lightblue", bd=0,
                     justify=RIGHT)
 input_field.grid(row=0, column=0)
 input_field.pack(ipady=10)
@@ -42,7 +43,7 @@ btns_frame.pack()
 # first row
 clear = Button(btns_frame, text="C/AC", fg="black", width=32, height=3, bd=0, bg="tomato",
                command=lambda: bt_clear()).grid(row=0, column=0, columnspan=3, padx=1, pady=1)
-divide = Button(btns_frame, text="÷", fg="black", width=10, height=3, bd=0, bg="lightblue",
+divide = Button(btns_frame, text="÷", fg="black", width=10, height=3, bd=0, bg="cyan",
                 command=lambda: btn_click("/")).grid(row=0, column=3, padx=1, pady=1)
 
 # second row
@@ -52,7 +53,7 @@ eight = Button(btns_frame, text="8", fg="black", width=10, height=3, bd=0, bg="l
                command=lambda: btn_click(8)).grid(row=1, column=1, padx=1, pady=1)
 nine = Button(btns_frame, text="9", fg="black", width=10, height=3, bd=0, bg="lightcyan",
               command=lambda: btn_click(9)).grid(row=1, column=2, padx=1, pady=1)
-multiply = Button(btns_frame, text="X", fg="black", width=10, height=3, bd=0, bg="lightblue",
+multiply = Button(btns_frame, text="X", fg="black", width=10, height=3, bd=0, bg="cyan",
                   command=lambda: btn_click("*")).grid(row=1, column=3, padx=1, pady=1)
 
 # third row
@@ -62,7 +63,7 @@ five = Button(btns_frame, text="5", fg="black", width=10, height=3, bd=0, bg="li
               command=lambda: btn_click(5)).grid(row=2, column=1, padx=1, pady=1)
 six = Button(btns_frame, text="6", fg="black", width=10, height=3, bd=0, bg="lightcyan", cursor="hand2",
              command=lambda: btn_click(6)).grid(row=2, column=2, padx=1, pady=1)
-minus = Button(btns_frame, text="−", fg="black", width=10, height=3, bd=0, bg="lightblue",
+minus = Button(btns_frame, text="−", fg="black", width=10, height=3, bd=0, bg="cyan",
                command=lambda: btn_click("-")).grid(row=2, column=3, padx=1, pady=1)
 
 # fourth row
@@ -72,7 +73,7 @@ two = Button(btns_frame, text="2", fg="black", width=10, height=3, bd=0, bg="lig
              command=lambda: btn_click(2)).grid(row=3, column=1, padx=1, pady=1)
 three = Button(btns_frame, text="3", fg="black", width=10, height=3, bd=0, bg="lightcyan", cursor="hand2",
                command=lambda: btn_click(3)).grid(row=3, column=2, padx=1, pady=1)
-plus = Button(btns_frame, text="+", fg="black", width=10, height=3, bd=0, bg="lightblue",
+plus = Button(btns_frame, text="+", fg="black", width=10, height=3, bd=0, bg="cyan",
               command=lambda: btn_click("+")).grid(row=3, column=3, padx=1, pady=1)
 
 # fourth row
@@ -80,7 +81,7 @@ zero = Button(btns_frame, text="0", fg="black", width=10, height=3, bd=0, bg="li
               command=lambda: btn_click(0)).grid(row=4, column=1, padx=1, pady=1)
 point = Button(btns_frame, text=".", fg="black", width=10, height=3, bd=0, bg="lightcyan",
                command=lambda: btn_click(".")).grid(row=4, column=0, padx=1, pady=1)
-equals = Button(btns_frame, text="=", fg="black", width=21, height=3, bd=0, bg="lightblue",
+equals = Button(btns_frame, text="=", fg="black", width=21, height=3, bd=0, bg="cyan",
                 command=lambda: bt_equal()).grid(row=4, column=2, columnspan=2, padx=1, pady=1)
 
 
