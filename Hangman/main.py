085aae8fda17dcca9097d83a16fd5be7e72356f7
Manish <27.maanish@gmail.com>
2021-09-10 11:25:11 +0545

    Screen and mouse position

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,11 +1,21 @@
 import pygame
-import os
 
+#display
 pygame.init()
 WIDTH, HEIGHT = 800, 500
-pygame.display.set_mode((WIDTH, HEIGHT))
+win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman!")
 
+# images
+images = []
+for i in range (7):
+    image = pygame.image.load("hangman"+str(i)+".png")
+    images.append(image)
+
+#game variables
+hangman_status = 0
+
+#gameloop
 FPS = 60
 clock = pygame.time.Clock()
 run = True
@@ -13,6 +23,9 @@ run = True
 while run:
     clock.tick(FPS)
 
+    win.fill("white")
+    win.blit(images[hangman_status],(100,75))
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
